79b8771887d5d477e953e3d1961c77242b698558
codersrabbi <pcrabbi2020@gmail.com>
2026-01-06 17:51:48 +0600

    hero Section 1/2 done

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { FaAward, FaRegListAlt } from 'react-icons/fa';
+import { FcManager } from 'react-icons/fc';
+import { FiArrowUpRight } from 'react-icons/fi';
+import { PiStudentFill } from 'react-icons/pi';
+import { Link } from 'react-router';
+
+const HeroSection = () => {
+    return (
+        <div className='grid grid-cols-3 bg-[#F5F5F5] px-20 pt-10'>
+            <div className='col-span-2 '>
+                <div className='flex items-center h-10 border-l-[#F68319] border-l-2 mb-5'>
+                    <p className='text-xl bg-white shadow-2xs inline-block px-8 p-1 '>জ্ঞানের আকাঙ্ক্ষা করো। সীমাহীনভাবে শিখো।</p>
+                </div>
+                <h2 className='text-[44px] font-semibold'>ব্রাইট একাডেমিক কেয়ারে স্বাগতম</h2>
+                <p className='text-[18px] mt-8'>সাফল্যের স্বর্ণালী শিখরে পৌঁছাতে আমরা দৃঢ় প্রতিজ্ঞ। আমরা জানি, প্রতিশ্রুতি রক্ষায় <br /> আমরা ব্যক্তি নয়, বিবেকের কাছে দায়বদ্ধ।</p>
+
+                <div className='mt-8 mb-8'>
+                    <button className='btn flex items-center text-xl font-normal border border-[#F68319] bg-transparent pr-[5px]'>
+                        <p>আমাদের সম্পর্কে জানুন</p>
+                        <div className='border border-[#F68319] p-[5px] rounded'>
+                            <FiArrowUpRight></FiArrowUpRight>
+                        </div>
+                    </button>
+                </div>
+
+                <div className='flex justify-between items-center border rounded p-[10px]'>
+                    <div className='flex justify-center items-center gap-4'>
+                        <PiStudentFill className='h-8 w-10 text-blue-600' />
+                        <div>
+                            <p className='text-2xl'>৩০০০+</p>
+                            <p className='text-[16px]'>সফল শিক্ষার্থী</p>
+                        </div>
+                    </div>
+                    <div className='flex justify-center items-center gap-4'>
+                        <PiStudentFill className='h-8 w-10 text-blue-600' />
+                        <div>
+                            <p className='text-2xl'>৪৫+</p>
+                            <p className='text-[16px]'>অভিজ্ঞ শিক্ষক</p>
+                        </div>
+                    </div>
+                    <div className='flex justify-center items-center gap-4'>
+                        <FaRegListAlt className='h-8 w-10 text-blue-600' />
+                        <div>
+                            <p className='text-2xl'>১৭+</p>
+                            <p className='text-[16px]'>বছরের অভিনজ্ঞতা</p>
+                        </div>
+                    </div>
+                    <div className='flex justify-center items-center gap-4'>
+                        <FaAward className='h-8 w-10 text-blue-600' />
+                        <div>
+                            <p className='text-2xl'>৩০০০+</p>
+                            <p className='text-[16px]'>সফল শিক্ষার্থী</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='grid-cols-1 bg-amber-300'>Rabbi Mia</div>
+        </div>
+    );
+};
+
+export default HeroSection;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,21 @@
 import { IoIosCall } from "react-icons/io";
+import HeroSection from "./HeroSection";
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+</style>
 
 const Home = () => {
     return (
+        <>
         <div className="bg-[#F68319] h-12 flex items-center">
             <marquee behavior="" direction="">
-                <div className="flex justify-center items-center gap-3 text-white text-[20px]">
+                <div className="flex justify-center items-center gap-3 text-white text-[20px] ">
                     নতুন ব্যাচ শুরু হচ্ছে। ভর্তি চলছে। সিমিত আসন। <IoIosCall />কল করুন: ০১৭১৬৬১১২০৮
                 </div>
             </marquee>
         </div>
+        <HeroSection></HeroSection>
+        </>
     );
 };
 
